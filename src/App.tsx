07c6e56fc8e9acf5783257c60da77aa571d7972e
devdavidias <davitdt206@gmail.com>
2025-12-23 15:17:24 -0300

    componente de logo com ícone e link criado

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,25 @@
-import "./styles/theme.css";
-import "./styles/global.css";
+
 import  Container  from "./components/Container";
 import  Heading  from "./components/Heading";
+import Logo from "./components/Logo";
+
+import "./styles/theme.css";
+import "./styles/global.css";
 
 function App() {
   return (
     <>
       <Container>
-        <Heading>logo</Heading>
+        <Logo/>  
       </Container>
 
       <Container>
         <Heading>Menu</Heading>
       </Container>
 
-      <div className="container-fluid">
-        <div className="container">
-          <div className="content">
-            <section>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error,
-              esse! Vel neque, alias doloremque, optio facere sunt maiores,
-              velit et ipsam nemo asperiores assumenda adipisci dicta laborum
-              inventore fuga culpa!
-            </section>
-          </div>
-        </div>
-      </div>
+
+
+    
     </>
   );
 }
